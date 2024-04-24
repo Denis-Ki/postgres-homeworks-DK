5e66fadd83aa72bf08690cb669b63d86f88cfe7f
Denis Ki <duzzz@bk.ru>
2024-04-24 07:49:16 +0300

    edit main.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,8 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+import os
+
+sql_bd_names = ['employees', 'customers', 'orders']
+data_file_names = ['employees', 'customers', 'orders']
+
