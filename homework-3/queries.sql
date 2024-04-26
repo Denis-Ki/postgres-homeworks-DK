b98de502de376a2736117756baa2ff15b5e4d452
Denis-Ki <156631140+Denis-Ki@users.noreply.github.com>
2024-04-26 17:49:01 +0300

    Update queries.sql
    add 4 request

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -44,3 +44,10 @@ WHERE customer_id NOT IN (SELECT DISTINCT customer_id
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц 
 -- (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+
+SELECT DISTINCT product_name
+FROM products
+WHERE product_id IN (SELECT product_id
+		     FROM order_details
+		     WHERE quantity = 10)
+ORDER BY product_name
